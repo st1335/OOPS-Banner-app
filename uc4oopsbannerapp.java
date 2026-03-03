e7062a973889815595b1246a321044f5a3f3e4bf
st1335 <st1335@srmist.edu.in>
2026-03-03 10:25:45 +0530

    Create uc4oopsbannerapp.java

---
+++ uc4oopsbannerapp.java
@@ -0,0 +1,44 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        String[] O = {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+
+        String[] P = {
+                " ******* ",
+                " *     * ",
+                " *     * ",
+                " ******* ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+
+        String[] S = {
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "        *",
+                " *     * ",
+                "  *****  "
+        };
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", O[i], O[i], P[i], S[i]);
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
